e6afd282a9a0ce0d30f7d0584f8a0a6b8a935bbf
kangyanf <kangyanf2008@163.com>
2024-04-08 16:44:20 +0800

    word2vec使用本地相对路径

--- src/word2vec/main.go
+++ src/word2vec/main.go
@@ -7,26 +7,32 @@ import (
 )
 
 func main() {
-	/*model, err := word2vec.New(
-		word2vec.Window(200),
-		word2vec.Model(word2vec.SkipGram),
-		word2vec.Optimizer(word2vec.NegativeSampling),
-		word2vec.NegativeSampleSize(5),
-		word2vec.Verbose(),
-	)
-	f, fe := os.Open("D:\\kyf\\go-workspace\\go-demo\\src\\word2vec\\data")
-	if fe != nil {
-		fmt.Println(fe)
+	/*
+		model, err := word2vec.New(
+				word2vec.Window(200),
+				word2vec.Model(word2vec.SkipGram),
+				word2vec.Optimizer(word2vec.NegativeSampling),
+				word2vec.NegativeSampleSize(5),
+				word2vec.Verbose(),
+			)
+			f, fe := os.Open(".//src//word2vec//data")
+			if fe != nil {
+				fmt.Println(fe)
+				return
+			}
+			defer f.Close()
+
+			fmt.Println("学习", model.Train(f))
+			modelFile, _ := os.OpenFile("..//src//word2vec//data.bin", os.O_CREATE, 0666)
+			fmt.Println(model.Save(modelFile, vector.Single))
+			modelFile.Close()
+	*/
+	modelFile2, modelFile2Err := os.OpenFile(".//src//word2vec//data.bin", os.O_RDONLY, 0666)
+	if modelFile2Err != nil {
+		fmt.Println(modelFile2Err)
 		return
 	}
-	defer f.Close()
 
-	fmt.Println("学习", model.Train(f))
-	modelFile, _ := os.OpenFile("D:\\kyf\\go-workspace\\go-demo\\src\\word2vec\\data.bin", os.O_CREATE, 0666)
-	fmt.Println(model.Save(modelFile, vector.Single))
-	modelFile.Close()*/
-
-	modelFile2, _ := os.OpenFile("D:\\kyf\\go-workspace\\go-demo\\src\\word2vec\\data.bin", os.O_RDONLY, 0666)
 	embed, err := embedding.Load(modelFile2)
 	modelFile2.Close()
 	fmt.Println(embed, err)
